2db911072410515ead4e363688d50cfdbfe8a727
Ramy Allam <linux@ramyallam.com>
2023-11-10 20:36:11 +0200

    Add deno

--- deno/index.js
+++ deno/index.js
@@ -4,4 +4,4 @@ import { parse } from 'https://deno.land/std@0.168.0/flags/mod.ts';
 const { args } = Deno;
 const argPort = Deno.env.get("PORT") ? Number(Deno.env.get("PORT")) : 8000;
 
-serve((_req) => new Response("Hello, world"), { port: argPort });
+serve({ hostname: "0.0.0.0", port: argPort }, (_req) => new Response("Hello, world"));
