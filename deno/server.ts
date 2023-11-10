cff4731691839d1cc781bc2545915a2b9db61ceb
Ramy Allam <linux@ramyallam.com>
2023-11-10 20:25:10 +0200

    Add deno

--- deno/server.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from "https://deno.land/std/http/server.ts";
-
-const server = serve({ hostname: "0.0.0.0", port: 8080 });
-console.log("HTTP web server running at http://0.0.0.0:8080/");
-
-server.forEach(async (req) => {
-  req.respond({ body: "Hello World\n" });
-});
