d3b1d2b3280286d1d199f5b87d543999d5fcf863
Ramy Allam <linux@ramyallam.com>
2023-11-10 19:18:05 +0200

    Update server.cr

--- src/server.cr
+++ src/server.cr
@@ -5,6 +5,6 @@ server = HTTP::Server.new do |context|
   context.response.print "Hello World"
 end
 
-address = server.bind_tcp 8080
+address = server.bind_tcp "0.0.0.0", 8080
 puts "Listening on http://#{address}"
 server.listen
