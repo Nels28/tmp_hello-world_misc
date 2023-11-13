40df30ee168af07d77454387d667df3aa3258725
Nelson Leiva <Nels28@users.noreply.github.com>
2023-11-13 00:20:30 -0800

    Create build.zig

---
+++ zig/build.zig
@@ -0,0 +1,11 @@
+const Builder = @import("std").build.Builder;
+
+pub fn build(b: *Builder) void {
+    const target = b.standardTargetOptions(.{});
+    const mode = b.standardReleaseOptions();
+
+    const exe = b.addExecutable("hello-world-server", "src/main.zig");
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+    exe.install();
+}
