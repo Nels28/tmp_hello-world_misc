9aebef8a3193b721074d0cd09aa8a6a99c922544
Nelson Leiva <nelsonleiva8@gmail.com>
2024-02-16 11:03:35 -0300

    updates

--- elixir2/start_server.exs
+++
@@ -1,9 +0,0 @@
-defmodule StartServer do
-  def start do
-    Application.ensure_all_started(:hello_world_web_server)
-
-    {:ok, _} = Plug.Adapters.Cowboy.http HelloWorldWebServer, []
-  end
-end
-
-StartServer.start()
