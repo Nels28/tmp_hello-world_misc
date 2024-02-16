dbe77fa8fee37c3551aeac05ac3b9c8c519fda28
Nelson Leiva <nelsonleiva8@gmail.com>
2024-02-16 10:56:13 -0300

    fix files

---
+++ elixir2/start_server.exs
@@ -0,0 +1,9 @@
+defmodule StartServer do
+  def start do
+    Application.ensure_all_started(:hello_world_web_server)
+
+    {:ok, _} = Plug.Adapters.Cowboy.http HelloWorldWebServer, []
+  end
+end
+
+StartServer.start()
